348c64fa0d760e01359e80db334621b3ecb7a6aa
tomasposse <tomasposse96@gmail.com>
2026-08-10 02:29:49 -0300

    fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({
-  site: 'https://www.tokyyto.com',
-});
+export default defineConfig(({ command }) => ({
+  site: command === 'build' ? 'https://tomasposse.github.io/website' : 'http://localhost:4321',
+  base: command === 'build' ? '/website' : '/',
+}));
